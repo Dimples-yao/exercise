c8f3d01003d996ecefbbbbd48157d926c85aa5f5
Lisy <yao211x@qq.com>
2018-04-14 09:18:26 +0800

    java编程思想

---
+++ ThinkingInJava/src/Eleven/ListFeatures.java
@@ -0,0 +1,10 @@
+package Eleven;
+
+import java.util.List;
+import java.util.Random;
+
+public class ListFeatures {
+    public static void main(String[] args) {
+        Random rand = new Random(47);
+    }
+}
