0c3c505d817318f07be23248da14cd53db7d75a1
Lisy <yao211x@qq.com>
2018-06-10 14:27:17 +0800

    未做

--- Solution/src/com/lisy/Solution.java
+++ Solution/src/com/lisy/Solution.java
@@ -745,5 +745,17 @@ public class Solution {
         return left!=null?left:pRootOfTree;
     }
 
+    /**
+     * 字符串的排列
+     * 时间限制：1秒 空间限制：32768K 热度指数：207351
+     * 输入一个字符串,按字典序打印出该字符串中字符的所有排列。例如输入字符串abc,则打印出由字符a,b,c所能排列出来的所有字符串abc,acb,bac,bca,cab和cba。
+     * 输入描述:
+     * 输入一个字符串,长度不超过9(可能有字符重复),字符只包括大小写字母。
+     */
+    public ArrayList<String> Permutation(String str) {
+        ArrayList<String> result = new ArrayList<>();
+        return result;
+    }
+
 }
 
