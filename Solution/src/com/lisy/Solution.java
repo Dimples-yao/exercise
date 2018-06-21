b7fa2841c6d200f89ba5f2fabf5d09a9d3cf3adb
Dimples-yao <yao211x@qq.com>
2018-06-21 10:37:08 +0800

    数组中出现次数超过一半的数字

--- Solution/src/com/lisy/Solution.java
+++ Solution/src/com/lisy/Solution.java
@@ -778,5 +778,26 @@ public class Solution {
         }
     }
 
+    /**
+     * 数组中出现次数超过一半的数字
+     * 数组中有一个数字出现的次数超过数组长度的一半，请找出这个数字。例如输入一个长度为9的数组{1,2,3,2,2,2,5,4,2}。由于数字2在数组中出现了5次，超过数组长度的一半，因此输出2。如果不存在则输出0。
+     */
+    public int MoreThanHalfNum_Solution(int [] array) {
+        Map<Integer,Integer> map = new HashMap<>();
+        for (int x:array){
+            if (map.containsKey(x)){
+                int i = map.get(x)+1;
+                map.put(x,i);
+            }else {
+                map.put(x,1);
+            }
+        }
+        for (int x:map.keySet()){
+            if (map.get(x)>(array.length/2))
+                return x;
+        }
+        return 0;
+    }
+
 }
 
--- Solution/tests/Main.java
+++ Solution/tests/Main.java
@@ -8,7 +8,7 @@ public class Main {
         /*int[] pre = {1,2,3,4,5,6,7};
         int[] in = {3,2,4,1,6,5,7};
         solution.reConstructBinaryTree(pre,in);*/
-        int[] test = {7,4,6,5};
-        System.out.println(solution.VerifySquenceOfBST(test));
+        int[] test = {1,2,3,2,2,2,5,4,2};
+        System.out.println(solution.MoreThanHalfNum_Solution(test));
     }
 }
