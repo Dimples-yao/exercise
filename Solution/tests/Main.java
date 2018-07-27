60a8199f9f9e0767370ee0b83baa105eb88136e5
Dimples-yao <yao211x@qq.com>
2018-07-27 11:15:57 +0800

    和为S的连续正数序列

--- Solution/tests/Main.java
+++ Solution/tests/Main.java
@@ -8,7 +8,7 @@ public class Main {
         /*int[] pre = {1,2,3,4,5,6,7};
         int[] in = {3,2,4,1,6,5,7};
         solution.reConstructBinaryTree(pre,in);*/
-        int[] test = {1,2,3,4,5,6,7,0};
-        System.out.println(solution.InversePairs(test));
+        int[] test = {2,4,3,6,3,2,5,5};
+        System.out.println(solution.FindContinuousSequence(9));
     }
 }
