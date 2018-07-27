5c0dec19592c44899083b6af8df2f2f4642432b0
Dimples-yao <yao211x@qq.com>
2018-07-27 10:25:35 +0800

    数组中只出现一次的数字

--- Solution/src/com/lisy/Solution.java
+++ Solution/src/com/lisy/Solution.java
@@ -1150,5 +1150,30 @@ public class Solution {
         return leftBoolean&&rightBoolean;
     }
 
+    /**
+     * 数组中只出现一次的数字
+     * 题目描述
+     * 一个整型数组里除了两个数字之外，其他的数字都出现了偶数次。请写程序找出这两个只出现一次的数字。
+     */
+    //num1,num2分别为长度为1的数组。传出参数
+    // 将num1[0],num2[0]设置为返回结果
+    public void FindNumsAppearOnce(int [] array,int num1[] , int num2[]) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        boolean isFirst = true;
+        for (int x:array) {
+            if (!map.containsKey(x))
+                map.put(x,1);
+            else map.put(x,map.get(x)+1);
+        }
+        for (int x:map.keySet()) {
+            if (map.get(x)==1){
+                if (isFirst){
+                    num1[0] = x;
+                    isFirst = false;
+                }else num2[0] = x;
+            }
+        }
+    }
+
 }
 
