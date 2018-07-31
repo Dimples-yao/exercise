c1e3215dcb7f2754ff20235107dfa14072b65d0f
Dimples-yao <yao211x@qq.com>
2018-07-31 14:00:24 +0800

    求1+2+3+...+n

--- Solution/src/com/lisy/Solution.java
+++ Solution/src/com/lisy/Solution.java
@@ -1338,5 +1338,16 @@ public class Solution {
         return list.get(0);
     }
 
+    /**
+     * 求1+2+3+...+n
+     * 题目描述
+     * 求1+2+3+...+n，要求不能使用乘除法、for、while、if、else、switch、case等关键字及条件判断语句（A?B:C）。
+     */
+    public int Sum_Solution(int n) {
+        int ans =n;
+        boolean t = ((ans!=0) && ((ans += Sum_Solution(n - 1))!=0));
+        return ans;
+    }
+
 }
 
