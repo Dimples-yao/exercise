05ca0abb383b83f34f7939344a1c893297d1a6ca
Dimples-yao <yao211x@qq.com>
2018-08-21 10:00:42 +0800

    构建乘积数组

--- Solution/src/com/lisy/Solution.java
+++ Solution/src/com/lisy/Solution.java
@@ -1457,5 +1457,24 @@ public class Solution {
         return false;
     }
 
+    /**
+     * 构建乘积数组
+     * 题目描述
+     * 给定一个数组A[0,1,...,n-1],请构建一个数组B[0,1,...,n-1],其中B中的元素B[i]=A[0]*A[1]*...*A[i-1]*A[i+1]*...*A[n-1]。不能使用除法。
+     */
+    public int[] multiply(int[] A) {
+        int[] B = new int[A.length];
+        for (int i=0;i<A.length;i++){
+            int sumExitI = 1;
+            for (int j=0;j<A.length;j++){
+                if (i!=j){
+                    sumExitI = sumExitI*A[j];
+                }
+            }
+            B[i] = sumExitI;
+        }
+        return B;
+    }
+
 }
 
--- Solution/tests/Main.java
+++ Solution/tests/Main.java
@@ -10,8 +10,8 @@ public class Main {
         /*int[] pre = {1,2,3,4,5,6,7};
         int[] in = {3,2,4,1,6,5,7};
         solution.reConstructBinaryTree(pre,in);*/
-        int[] test = {2,1,3,1,4};
+        int[] test = {1,2,3,4,5};
         int [] duplication = new int[test.length];
-        System.out.println(solution.duplicate(test,test.length,duplication));
+        System.out.println(solution.multiply(test));
     }
 }
