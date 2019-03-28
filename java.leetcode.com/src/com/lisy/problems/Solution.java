53b231a8612c321fb3575252badce49bb9a2ffe1
Dimples-yao <yao211x@qq.com>
2019-03-28 10:59:40 +0800

    ceshi

--- java.leetcode.com/src/com/lisy/problems/Solution.java
+++ java.leetcode.com/src/com/lisy/problems/Solution.java
@@ -7,5 +7,28 @@
  */
 package com.lisy.problems;
 
+import java.util.*;
+
 public class Solution {
+
+    /**
+     * 卡牌分组：https://leetcode-cn.com/problems/x-of-a-kind-in-a-deck-of-cards/
+     * @param deck
+     * @return boolean
+     */
+    public boolean hasGroupsSizeX(int[] deck) {
+        if (deck.length<2)
+            return false;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for (int i : deck) {
+            map.put(i,map.getOrDefault(i,0)+1);
+        }
+        int tem = deck[0];
+        for (int i: map.keySet()) {
+            if (map.get(i) != map.get(tem))
+                return false;
+        }
+        return true;
+    }
+
 }
---
+++ java.leetcode.com/test/Test.java
@@ -0,0 +1,17 @@
+import com.lisy.problems.Solution;
+
+/**
+ * Copyright © 贝壳找房（北京）科技有限公司.
+ * User: lishaoyao001
+ * Date: 2019-03-27
+ * Time: 21:52
+ * Desc:
+ */
+
+public class Test {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] intArray = {1,1,2,3};
+        System.out.println(solution.hasGroupsSizeX(intArray));
+    }
+}
